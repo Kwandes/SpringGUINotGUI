ac12d38673006bdaba2a31698463059145f2af23
Jan <jan.bogoryja@gmail.com>
2020-04-26 20:42:31 +0200

    Add basic Person model
    Add a basic class that represents a single person.
    Will be expanded to account for the Spring SQL framework (repository etc)

---
+++ src/main/java/fail/faisal/springExercise/Model/Person.java
@@ -0,0 +1,58 @@
+/*
+    Basic class to hold a single database record
+    Will be expanded as other Spring SQL functionality is added
+ */
+
+package fail.faisal.springExercise.Model;
+
+public class Person
+{
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person()
+    {
+    }
+
+    public Person(int id, String firstName, String lastName)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //region Getters and Setters
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    //endregion
+}
